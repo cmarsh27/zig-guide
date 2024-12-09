b9e37e1cf5b49264d3438f621874f7b5b3ccac85
cmarsh27 <calumpm111@googlemail.com>
2024-12-09 16:20:01 +0000

    if expressions

--- main.zig
+++ main.zig
@@ -1,5 +1,17 @@
 const std = @import("std");
 
+//const and var
+const constant: i32 = 5; // signed 32-bit constant
+var variable: u32 = 5000; // unsigned 32-bit variable
+
+// @as performs an explicit type coercion
+const inferred_constant = @as(i32, 5);
+var inferred_variable = @as(u32, 5000);
+
+//Arrays
+const array = [_]u8{ 'h', 'e', 'l', 'l', 'o' };
+const length = array.len; // 5
+
 pub fn main() void {
     std.debug.print("Hello, {s}!\n", .{"World"});
 }
