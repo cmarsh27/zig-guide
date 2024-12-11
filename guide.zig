3a52ca14701ad07e3cdd22d71265efdb8ffa6c56
cmarsh27 <calumpm111@googlemail.com>
2024-12-11 16:48:59 +0000

    Switch

--- guide.zig
+++ guide.zig
@@ -171,3 +171,29 @@ test "inferred error set" {
 const A = error{ NotDir, PathNotFound };
 const B = error{ OutOfMemory, PathNotFound };
 const C = A || B;
+
+test "switch statement" {
+    var x: i8 = 10;
+    switch (x) {
+        -1...1 => {
+            x = -x;
+        },
+        10, 100 => {
+            //special considerations must be made
+            //when dividing signed integers
+            x = @divExact(x, 10);
+        },
+        else => {},
+    }
+    try expect(x == 1);
+}
+
+test "switch expression" {
+    var x: i8 = 10;
+    x = switch (x) {
+        -1...1 => -x,
+        10, 100 => @divExact(x, 10),
+        else => x,
+    };
+    try expect(x == 1);
+}
